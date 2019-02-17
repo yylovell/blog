1cfba2cc2d022ef79fa6899d55ef6253f5e4064a
yylovell <yyforll@126.com>
2019-02-17 14:47:06 +0800

    test

--- src/main/java/com/yy/blog/controller/HelloController.java
+++ src/main/java/com/yy/blog/controller/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+
     @GetMapping(value = "/hello")
     public String hello() {
         return "Hello World";
